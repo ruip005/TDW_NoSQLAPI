631a422dc6999decfa0f7d7b9d2f9fde32a9cbc1
Rui Rodrigues <pv28292@alunos.estgv.ipv.pt>
2024-01-16 11:54:55 +0000

    Delete Routes directory

--- Routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const pratosController = require('../Controllers/pratos');
-
-router.post('/create', pratosController.create);
-router.get('/all', pratosController.all);
-router.put('/edit/:id', pratosController.edit);
-router.delete('/delete/:id', pratosController.delete);
-
-module.exports = router;
